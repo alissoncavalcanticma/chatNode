dab209be937b5345b804be9777bc3b2b3ecb547d
Alisson Cavalcanti Galvão <alisson.galvao@amcom.com.br>
2023-09-04 00:01:17 -0300

    Login no chat

--- public/main.js
+++ public/main.js
@@ -2,3 +2,50 @@
 //se fosse fazer a conexão com um server específico, deveria especificar ele na função: 
 //const socket = io(localhost.......);
 const socket = io();
+
+let userName = '';
+let userList = [];
+
+let LoginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+function renderList() {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
+
+    userList.forEach(i => {
+        ul.innerHTML += '<li>' + i + '</li>'
+    });
+}
+
+//socket para login no chat
+loginInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        let name = loginInput.value.trim();
+        if (name != '') {
+            userName = name;
+            document.title = 'Chat (' + userName + ')';
+
+            socket.emit('join-request', userName);
+        }
+    }
+});
+
+//escuta para ativar o chat após o login e reposta do server
+
+socket.on('user-ok', (list) => {
+    LoginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    //iniciar com foco no component chatTextInput
+    textInput.focus();
+
+    userList = list;
+    //função para renderizar a lista de users ativos
+    renderList();
+});
