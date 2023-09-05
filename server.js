61962b4f35afb87d25c6a29e3e6fa1b1aa2bc85e
Alisson Cavalcanti Galvão <alisson.galvao@amcom.com.br>
2023-09-04 23:15:34 -0300

    Listando status de  usuários conectados e desconectados

--- server.js
+++ server.js
@@ -28,5 +28,23 @@ io.on('connection', (socket) => {
         console.log(connectedUsers);
 
         socket.emit('user-ok', connectedUsers);
+
+        socket.broadcast.emit('list-update', {
+            joined: userName,
+            list: connectedUsers
+        })
+    });
+
+    //Disconect users
+
+    socket.on('disconnect', () => {
+
+        connectedUsers = connectedUsers.filter(u => u != socket.userName);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update', {
+            left: socket.userName,
+            list: connectedUsers
+        });
     });
 });
